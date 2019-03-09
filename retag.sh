2ddd5cb3fa58395589146c6555ca9c17ed73b6c7
Pau Rodriguez-Estivill <prodrigestivill@gmail.com>
2019-03-09 20:21:58 +0100

    Fixed alpine builds

--- retag.sh
+++ retag.sh
@@ -2,14 +2,14 @@
 TAGS="11 10 9.6 9.5 9.4"
 
 git push --tags -f
+git reset --hard
 for TAG in $TAGS; do
-	git reset --hard
-	sed -i "1 s,\$,:$TAG," Dockerfile-debian
-	sed -i "1 s,\$,-$TAG," Dockerfile-alpine
+	sed -i "1 s,\$,:${TAG}," Dockerfile-debian
+	sed -i "1 s,:alpine\$,:${TAG}-alpine," Dockerfile-alpine
 	git commit -m "Tag $TAG" Dockerfile-*
 	git tag -f "$TAG" 
 	# github events needs to push each tag separately
 	git push --tags -f
 	git reset HEAD^
+	git reset --hard
 done
-git reset --hard
