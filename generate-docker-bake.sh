f0b5949af03d9a22da284a605724544156fecfb1
Pau Rodriguez-Estivill <prodrigestivill@gmail.com>
2022-03-04 16:22:52 +0100

    Rename generate docker bake script

--- generate-docker-bake.sh
+++ generate-docker-bake.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-DOCKER_BAKE_FILE=${1:-"docker-bake.hcl"}
-
 GOCRONVER="v0.0.10"
 MAIN_TAG="14"
 TAGS_EXTRA="13 12"
 PLATFORMS="linux/amd64 linux/arm64 linux/arm/v7 linux/s390x linux/ppc64le"
 TAGS_EXTRA_2="11 10"
 PLATFORMS_DEBIAN_2="linux/amd64 linux/arm64 linux/arm/v7"
-
+DOCKER_BAKE_FILE="docker-bake.hcl"
 
 cd "$(dirname "$0")"
 
